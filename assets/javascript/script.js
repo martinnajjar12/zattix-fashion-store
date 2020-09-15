7d14ef18854b35ecebf4c0ada0529ad275c06ff1
unknown <martin_najjar@yahoo.com>
2020-09-15 19:38:36 +0300

    Adding Advertisement section

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+// Brands' Carousel
+
 $(".carousel .carousel-item").each(function () {
   var minPerSlide = 3;
   var next = $(this).next();
@@ -15,3 +17,48 @@ $(".carousel .carousel-item").each(function () {
     next.children(":first-child").clone().appendTo($(this));
   }
 });
+
+// Advertisements' Carousel
+
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselDivs = document.querySelectorAll(".carousel-slide>div");
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+let counter = 1;
+const size = carouselDivs[0].clientWidth;
+
+carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+
+nextBtn.addEventListener("click", moveForward);
+prevBtn.addEventListener("click", moveBackward);
+
+function moveForward() {
+  if (counter >= carouselDivs.length - 1) return;
+  carouselSlide.style.transition = "transform 0.4s";
+  counter++;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+}
+
+function moveBackward() {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s";
+  counter--;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+}
+
+carouselSlide.addEventListener("transitionend", checkDivId);
+
+function checkDivId() {
+  if (carouselDivs[counter].id === "lastClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselDivs.length - 2;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
+
+  if (carouselDivs[counter].id === "firstClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselDivs.length - counter;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
+}
